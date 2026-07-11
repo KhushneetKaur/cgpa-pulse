ad7a1a10640e9bc90f74535a0a9f3611e4195308
KhushneetKaur <khushneetgirgla@gmail.com>
2026-07-11 10:41:26 +0530

    Fix : Google Signup1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import "./styles/responsive.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}></GoogleOAuthProvider>
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
